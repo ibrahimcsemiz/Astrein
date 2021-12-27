3a62d0856a182cce5d7710b1bd35b41327c82ba4
Icansemiz <ibrahimcansemiz@yandex.com>
2021-12-27 17:24:53 +0100

    Add new method

--- app/Http/Livewire/UserComponent.php
+++ app/Http/Livewire/UserComponent.php
@@ -12,6 +12,14 @@ class UserComponent extends Component
 
     public $search = '';
     public $function = '';
+    public $sortField = 'id';
+    public $sortDirection = 'asc';
+
+    public $queryString = [
+        'search' => ['except' => ''],
+        'function' => ['except' => ''],
+        'page' => ['except' => 1]
+    ];
 
     public function updatingSearch()
     {
@@ -23,10 +31,18 @@ class UserComponent extends Component
         $this->resetPage();
     }
 
+    public function sortBy($field)
+    {
+        $this->sortDirection = $this->sortField === $field
+            ? $this->sortDirection = $this->sortDirection === 'asc' ? 'desc' : 'asc'
+            : 'asc';
+
+        $this->sortField = $field;
+    }
+
     public function render()
     {
-        $users = User::orderByDesc('created_at')
-            ->when($this->function, function ($users) {
+        $users = User::when($this->function, function ($users) {
                 $users->where('function', '=', $this->function);
             })
             ->when($this->search, function ($users) {
@@ -43,6 +59,7 @@ class UserComponent extends Component
             })
             ->with('contact')
             ->with('personal')
+            ->orderBy($this->sortField, $this->sortDirection)
             ->paginate(25);
 
         return view('livewire.user-component', [
