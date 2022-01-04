cdcd9135085aec0c12898088d3ed958fb9f4e101
Icansemiz <ibrahimcansemiz@yandex.com>
2022-01-04 14:09:59 +0100

    Update User Controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,8 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\User\StoreUserRequest;
-use App\Http\Requests\User\UpdateUserRequest;
+use App\Http\Requests\UserRequest;
 use App\Models\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -11,57 +10,40 @@ use Illuminate\Support\Str;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return view('users.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StoreUserRequest $request)
+    public function store(UserRequest $request)
     {
         $password = Str::random(8);
 
         $response = DB::transaction(function() use ($request, $password) {
             $insertUser = User::create([
-                'name' => Str::title($request->input('name')),
-                'email' => $request->input('email'),
-                'function' => 'Employee',
-                // 'function' => $request->input('function'),
+                'name' => $request->name,
+                'email' => $request->email,
+                'function' => $request->function,
                 'password' => Hash::make($password)
             ]);
 
             // Creating Contact Information
 
             $insertContactInformation = $insertUser->contact()->create([
-                'telephone' => $request->input('telephone'),
-                'city' => $request->input('city'),
-                'address' => $request->input('address')
+                'telephone' => $request->telephone,
+                'city' => $request->city,
+                'address' => $request->address
             ]);
 
             // Creating Personal Information
 
             $insertPersonalInformation = $insertUser->personal()->create([
-                'birth_date' => $request->input('birth_date')
+                'birth_date' => $request->birth_date
             ]);
 
             if ($insertUser && $insertContactInformation && $insertPersonalInformation) {
@@ -78,72 +60,40 @@ class UserController extends Controller
         }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
-     */
-    public function edit($id)
+    public function edit(User $user)
     {
-        $user = User::exists($id);
+        $user->load(['contact', 'personal']);
 
-        if (!$user) {
-            return redirect(url('users'))->with('status', 'error')->with('message', 'User not found.');
-        }
-
-        return view('users.edit', [
-            'data' => $user->with('contact')->with('personal')->get()
-        ]);
+        return view('users.edit', compact('user'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
-     */
-    public function update(UpdateUserRequest $request, $id)
+    public function update(UserRequest $request, User $user)
     {
-        $user = User::exists($id);
-
-        if (!$user) {
-            return redirect(url('users'))->with('status', 'error')->with('message', 'User not found.');
-        }
-
         $response = DB::transaction(function() use ($user, $request) {
 
             $updateUser = $user->update([
-                'name' => Str::title($request->input('name')),
-                'email' => $request->input('email'),
-                'function' => 'Employee',
-                // 'function' => $request->input('function')
+                'name' => $request->name,
+                'email' => $request->email,
+                'function' => $request->function
             ]);
 
             // Updating Contact Information
 
             $updateContactInformation = $user->contact()->update([
-                'telephone' => $request->input('telephone'),
-                'city' => $request->input('city'),
-                'address' => $request->input('address')
+                'telephone' => $request->telephone,
+                'city' => $request->city,
+                'address' => $request->address
             ]);
 
             // Updating Personal Information
 
             $updatePersonalInformation = $user->personal()->update([
-                'birth_date' => $request->input('birth_date')
+                'birth_date' => $request->birth_date
             ]);
 
             if ($updateUser && $updateContactInformation && $updatePersonalInformation) {
@@ -160,20 +110,8 @@ class UserController extends Controller
         }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
-     */
-    public function destroy($id)
+    public function destroy(User $user)
     {
-        $user = User::exists($id);
-
-        if (!$user) {
-            return redirect(url('users'))->with('status', 'error')->with('message', 'User not found.');
-        }
-
         $deleteUser = $user->delete();
 
         if ($deleteUser) {
