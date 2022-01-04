e06acdabb8bc1ca2c92d8c01886a3a08298341ae
Icansemiz <ibrahimcansemiz@yandex.com>
2022-01-05 00:21:05 +0100

    Update Layout

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -17,8 +17,8 @@
 
                 <x-slot name="links">
                     <x-navigation.link :href="route('dashboard')" :active="Route::is('dashboard')">{{ __('Dashboard') }}</x-navigation.link>
-                    <x-navigation.link :href="route('users')" :active="Route::is('users/*')">{{ __('Users') }}</x-navigation.link>
-                    <x-navigation.link :href="route('users')" :active="Route::is('users')">{{ __('Employees') }}</x-navigation.link>
+                    <x-navigation.link :href="route('users')" :active="Route::is('users')">{{ __('Users') }}</x-navigation.link>
+                    <x-navigation.link :href="route('employees')" :active="Route::is('employees')">{{ __('Employees') }}</x-navigation.link>
                     <x-navigation.link :href="route('hotels')" :active="Route::is('hotels')">{{ __('Hotels') }}</x-navigation.link>
                 </x-slot>
 
@@ -54,8 +54,8 @@
 
         <x-slot name="links">
             <x-navigation.link class="block text-base" :href="route('dashboard')" :active="Route::is('dashboard')">{{ __('Dashboard') }}</x-navigation.link>
-            <x-navigation.link class="block text-base" :href="route('users')" :active="Route::is('users/*')">{{ __('Users') }}</x-navigation.link>
-            <x-navigation.link class="block text-base" :href="route('users')" :active="Route::is('users')">{{ __('Employees') }}</x-navigation.link>
+            <x-navigation.link class="block text-base" :href="route('users')" :active="Route::is('users')">{{ __('Users') }}</x-navigation.link>
+            <x-navigation.link class="block text-base" :href="route('employees')" :active="Route::is('employees')">{{ __('Employees') }}</x-navigation.link>
             <x-navigation.link class="block text-base" :href="route('hotels')" :active="Route::is('hotels')">{{ __('Hotels') }}</x-navigation.link>
         </x-slot>
 
