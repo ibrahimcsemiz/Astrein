8a690c289a2128fe9e31980ba3996db0b63c8a18
Icansemiz <ibrahimcansemiz@yandex.com>
2022-01-11 10:47:11 +0100

    Label Component Fix

--- resources/views/components/form/label.blade.php
+++ resources/views/components/form/label.blade.php
@@ -1,5 +1,5 @@
 @props(['required'])
 
 <label {{ $attributes->merge(['class' => 'block text-sm font-medium text-gray-700']) }}>
-    {{ $slot ?? '' }} @if($required ?? '') {!! '<span class="text-red-400">(*)</span>' !!} @endif
+    {{ $slot ?? '' }} @if($required ?? '') {!! '*' !!} @endif
 </label>
