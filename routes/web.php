d6f22552808b0326cc7986d239783c37edc10ed0
Icansemiz <ibrahimcansemiz@yandex.com>
2022-01-14 13:38:44 +0100

    Update Route

--- routes/web.php
+++ routes/web.php
@@ -20,13 +20,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::group(['middleware' => 'auth'], function () {
 
-    Route::get('/dashboard', function () {
+    Route::get('/', function () {
         return view('dashboard');
     })->name('dashboard');
 
